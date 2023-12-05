a9df7df7174dff2b14899ee11f68f9dfe391289d
Abrshhh <abreham.dessiem@gmail.com>
2023-12-05 15:15:44 -0800

    defined all common attributes/methods of the class

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-from datetime import datetime
+import datetime
 
 '''Define a class BaseModel.'''
 
@@ -10,9 +10,20 @@ class BaseModel:
     def __init__(self):
         '''Initializes the class BaseModel.'''
 
-        self.id = uuid.uuid4()
-        self.created_at = datetime.now()
-        self.updated_at =
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
 
-        def __str__(self):
-            print(f"[BaseModel] {self.id} {self.__dict__}")
+    def __str__(self):
+        print(f"[BaseModel] ({self.id}) {self.__dict__}")
+
+    def save(self):
+        '''Updates updated_at with the current datetime.'''
+
+        self.updated_at = datetime.datetime.now()
+
+    def to_dict(self):
+        '''Returns a dictionary containing all keys/values
+        of __dict__ of the instance.'''
+
+        self.__dict__ = __dict__
