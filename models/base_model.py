cfc22a53c1200eb9931e19a94f276ba9ea919bbe
Abrshhh <abreham.dessiem@gmail.com>
2023-12-06 11:09:29 -0800

    task 3

--- models/base_model.py
+++ models/base_model.py
@@ -7,12 +7,15 @@ import datetime
 class BaseModel:
     '''Represents the class BaseModel.'''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initializes the class BaseModel.'''
 
         self.id = str(uuid.uuid4())
         self.created_at = datetime.datetime.now()
         self.updated_at = datetime.datetime.now()
+
+        if len(kwargs) != 0:
+
 
     def __str__(self):
         print(f"[BaseModel] ({self.id}) {self.__dict__}")
@@ -26,4 +29,9 @@ class BaseModel:
         '''Returns a dictionary containing all keys/values
         of __dict__ of the instance.'''
 
-        self.__dict__ = __dict__
+        self.created_at = self.created_at.isoformat()
+        self.updated_at = self.updated_at.isoformat()
+        self.__dict__ = dict(id=str(uuid.uuid4())
+                             created_at=self.created_at.isoformat()
+                             updated_at=self.updated_at.isoformat()
+                             )
