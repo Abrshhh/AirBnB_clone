9ae562385d3c675d3777d61a67e0fe2d6d508d73
Abrshhh <abreham.dessiem@gmail.com>
2023-12-07 04:05:36 -0800

    task 3 completed: to_dict method written

--- models/base_model.py
+++ models/base_model.py
@@ -15,7 +15,7 @@ class BaseModel:
         self.updated_at = datetime.datetime.now()
 
         if len(kwargs) != 0:
-
+            pass
 
     def __str__(self):
         print(f"[BaseModel] ({self.id}) {self.__dict__}")
@@ -29,9 +29,8 @@ class BaseModel:
         '''Returns a dictionary containing all keys/values
         of __dict__ of the instance.'''
 
-        self.created_at = self.created_at.isoformat()
-        self.updated_at = self.updated_at.isoformat()
-        self.__dict__ = dict(id=str(uuid.uuid4())
-                             created_at=self.created_at.isoformat()
-                             updated_at=self.updated_at.isoformat()
-                             )
+        return {"id": self.id,
+                "__class__": self.__class__.__name__,
+                "self.created_at": self.created_at.isoformat(),
+                "self.updated_at": self.updated_at.isoformat()
+                }
