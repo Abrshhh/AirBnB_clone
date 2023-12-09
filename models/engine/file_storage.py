178845265d51bfaffa49d033d3120b6b18282c60
Abrshhh <abreham.dessiem@gmail.com>
2023-12-09 04:46:04 -0800

    added save method

---
+++ models/engine/file_storage.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+import json
+from models.base_model import BaseModel
+'''Define a FileStorage class.'''
+
+
+class FileStorage:
+    '''Represents a FileStorage class that  serializes instances
+    to a JSON file and deserializes JSON file to instances.'''
+
+    def __init__(self):
+        '''Initialize the class FileStorage.'''
+        self.__file_path = "file.json"
+        self.__objects = {}
+
+    def all(self):
+        ''' '''
+        return self.__objects
+
+    def new(self, obj):
+        ''' '''
+        key = f"{obj.__class__.__name__}{obj.id}"
+        self.__objects[key] = obj
+
+    def save(self):
+        '''Serializes __objects to the JSON file .'''
+        new_dict = {}
+        for key, value in self.__objects.items():
+            new_dict[key] = value.to_dict()
+        with open(self.__file_path, 'w') as f:
+            json.dump(new_dict, f)
