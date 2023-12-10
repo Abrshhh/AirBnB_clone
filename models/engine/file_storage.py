0b98da1267c1bb560ba43728faee81eaf1277675
Abrshhh <abreham.dessiem@gmail.com>
2023-12-10 10:00:13 -0800

    added reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -12,28 +12,40 @@ from models.review import Review
 
 class FileStorage:
     '''Represents a FileStorage class that  serializes instances
-    to a JSON file and deserializes JSON file to instances.'''
+    to a JSON file and deserializes JSON file to instances.
 
-    def __init__(self):
-        '''Initialize the class FileStorage.'''
-        self.__file_path = "file.json"
-        self.__objects = {}
+    Attributes:
+        __file_path (str): The name of the file to save objects to.
+        __objects (dict): A dictionary of instantiated objects.
+    '''
+
+        __file_path = "file.json"
+        __objects = {}
 
     def all(self):
-        ''' '''
-        return self.__objects
+        '''Return the dictionary __objects.'''
+        return FileStorage.__objects
 
     def new(self, obj):
-        ''' '''
-        key = f"{obj.__class__.__name__}{obj.id}"
-        self.__objects[key] = obj
+        """Set in __objects obj with key <obj_class_name>.id"""
+        ocname = obj.__class__.__name__
+        FileStorage.__objects["{}.{}".format(ocname, obj.id)] = obj
 
     def save(self):
-        '''Serializes __objects to the JSON file .'''
-        new_dict = {}
+        """Serialize __objects to the JSON file __file_path."""
+        odict = FileStorage.__objects
+        objdict = {obj: odict[obj].to_dict() for obj in odict.keys()}
+        with open(FileStorage.__file_path, "w") as f:
+            json.dump(objdict, f)
 
-        for key, value in self.__objects.items():
-            new_dict[key] = value.to_dict()
-
-        with open(self.__file_path, 'w') as f:
-            json.dump(new_dict, f)
+    def reload(self):
+        """Deserialize the JSON file __file_path to __objects, if it exists."""
+        try:
+            with open(FileStorage.__file_path) as f:
+                objdict = json.load(f)
+                for o in objdict.values():
+                    cls_name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(cls_name)(**o))
+        except FileNotFoundError:
+            return
